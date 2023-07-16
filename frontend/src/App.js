879499600bc2fdc614058778de258a0742f8e053
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-07-16 22:54:29 +0300

    1

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      a
+          <Button variant="success">Success</Button>{' '}
+      </header>
+    </div>
+  );
+}
+
+export default App;
